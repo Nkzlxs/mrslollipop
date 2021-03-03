57a59de068af648c601dde447f25d3cd638b3e0a
nkzlxs <ngzhexuan2@gmail.com>
2021-03-04 01:40:54 +0800

    code refactor

---
+++ discord_bot/bot.py
@@ -0,0 +1,202 @@
+import discord
+import os
+import json
+import asyncio
+from datetime import datetime, date, timedelta
+import time
+import argparse
+
+import covid19
+from database_controller import db_controller_bot
+from database_controller import db_controller
+import random_ja
+import random_en
+
+from discord_slash import SlashCommand
+
+
+registered_channels = []
+
+def main():
+
+    credentials = readCrediential()
+
+    # client = discord.Client(
+    #     intents=discord.Intents(
+    #         guilds=True,
+    #         members=True,
+    #         messages=True
+    #     )
+    # )
+    client = discord.Client(
+        intents=discord.Intents.all()
+    )
+
+    @client.event
+    async def on_ready():
+        print('Logged on as {0}!'.format(client.user))
+        await client.change_presence(
+            activity=discord.Game(name="Online 4.0"),
+        )
+
+        client.loop.create_task(check_Covid19_routine(client))
+
+    @client.event
+    async def on_guild_join(guild):
+        print(f"i joined {guild.name}")
+        print(guild.id)
+        d = ""
+        async for m in guild.fetch_members(limit=1000):
+            d += m.name + "\n"
+        print(d)
+
+    @client.event
+    async def on_guild_remove(guild):
+        print(f"i left {guild.name}")
+
+    createSlashCommands(client)
+
+    client.run(credentials['DISCORD_BOT_ACCESS_TOKEN'])
+
+
+def createSlashCommands(client):
+    slash = SlashCommand(
+        client,
+        sync_commands=True
+    )
+
+    @slash.slash(
+        name="ping", 
+        description="Check your ping with the bot"
+    )
+    async def _ping(ctx):
+        await ctx.respond()
+        await ctx.send(f"Pong! ({client.latency*1000}ms)")
+
+    @slash.slash(
+        name="anchor", 
+        description="This command tells the bot where to post Covid-19 messages"
+    )
+    async def _anchor(ctx):
+        await ctx.respond()
+        await ctx.send("This channel: %s have been registered for information!"%ctx.channel.name)
+        registered_channels.append(ctx.channel.id)
+
+    @slash.slash(
+        name="deanchor", 
+        description="This command tells the bot to stop posting Covid-19 messages to this channel"
+    )
+    async def _deanchor(ctx):
+        await ctx.respond()
+        await ctx.send("This channel: %s have removed for information!"%ctx.channel.name)
+        registered_channels.pop(registered_channels.index(ctx.channel.id))
+
+
+async def check_Covid19_routine(client):
+
+    while True:
+
+        gmttime = time.gmtime(time.time())
+
+        print(gmttime.tm_min,gmttime.tm_sec)
+        if gmttime.tm_min % 30 == 0 and gmttime.tm_sec == 0:
+            await update_covid19(client)
+
+        await asyncio.sleep(1)
+
+# async def update_covid19(client):
+#     for _ in registered_channels:
+#         a_channel = client.get_channel(_)
+#         await a_channel.send(
+#             content="test",
+#         )
+
+async def update_covid19(client):
+    """ Get information from web fetcher program"""
+    latest_info = covid19.main()
+
+    """ Process the fetched infomation with the database """
+    a_db = db_controller(
+        cured=latest_info['cured'],
+        new=latest_info['new'],
+        death=latest_info['death']
+    )
+    response = a_db.fetch_sqldata_n_compare()
+
+    """ Decide to post or not """
+    if response['status'] is True:
+        """ Modify the description """
+        desc = ""
+        desc += ":chart_with_upwards_trend:Confirmed Count: %d (%+d)\n" % (
+            latest_info['new'], response['d_new'])
+        desc += ":sparkling_heart:Cured Count: %d (%+d)\n" % (
+            latest_info['cured'], response['d_cured'])
+        desc += ":skull:Death Count: %d (%+d)\n" % (
+            latest_info['death'], response['d_death'])
+        desc += ":hospital:Active Count: %d (%+d)\n" % (
+            response['c_active_cases'], response['d_active_cases'])
+
+        title = "COVID-19 Status in\n马来西亚 Malaysia"
+        embed_obj = discord.Embed(
+            title=title,
+            description=desc,
+            url=latest_info['article_src']
+        )
+        embed_obj.set_image(url=latest_info['image_src'])
+        embed_obj.set_footer(
+            text="From Kpkesihatan"
+        )
+
+        for _ in registered_channels:
+            a_channel = client.get_channel(_)
+            await a_channel.send(
+                content="@everyone Hi",
+                embed=embed_obj
+            )
+    else:
+        print(f"There's no update! {response['status']}")
+
+
+def readCrediential():
+    cred_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), "credential.json")
+    with open(cred_path, "r") as f:
+        credentials = json.load(f)
+
+    return credentials
+
+
+tests = [
+
+]
+
+
+def argParsing():
+    argparser = argparse.ArgumentParser(
+        description="Covid19 reporter bot of Nkzlxs's Discord Server.")
+    argparser.add_argument("--main", action="count",
+                           help="Append this to command for running the main bot")
+
+    available_function = ""
+    for f in tests:
+        available_function += str(f) + ","
+    available_function = available_function.strip(",")
+
+    argparser.add_argument(
+        "--test",
+        action="store",
+        metavar="TEST-FUNCTION",
+        help=f"Append this to command for testing; Choice: [{available_function}]",
+        choices=tests
+    )
+    result = argparser.parse_args()
+    if(result.main == None):
+        if result.test in tests:
+            globals()[result.test]()
+
+    if(result.main == 1 and result.test == None):
+        main()
+
+
+if __name__ == "__main__":
+    argParsing()
---
+++ discord_bot/covid19.py
@@ -0,0 +1,130 @@
+import requests
+import re
+
+def main():
+    answer = {}
+
+    """ Navigate to main page """
+    response = requests.get(url="https://kpkesihatan.com/")
+    res_text = response.text
+
+    """ Find the div with id main-content """
+    maincontent_index = res_text.find("<div id=\"main-content\">")
+
+    """ Find the hyperlinks, which are also the first element in each article """
+    a_link = res_text.find("<a href=",maincontent_index)
+    zeroth_link = res_text.find("<a href=",maincontent_index) # let the program remember the first <a href= position
+
+    while True:
+        """ Find the first and last quote that contains the link to the latest case """
+        first_quote = res_text.find("\"",a_link)
+        end_quote = res_text.find("\"",first_quote+1)
+
+        dif = end_quote - first_quote
+
+        """ Verify if it is about the latest covid19 information or not """
+        keywords = "situasi-semasa-jangkitan-penyakit-coronavirus-2019-covid-19-di-malaysia"
+        verify_word = res_text.find(keywords,first_quote,end_quote)
+        if verify_word == -1:
+            """ If there's no keywords in the first hyperlink, skip to the next one """
+            a_link = res_text.find("<a href=",a_link+len("<a href="))
+
+            """ When the search for "<a href=" returned to the beginning """
+            if a_link == zeroth_link:
+                exit("No specifed keywords found! Exiting...") # terminate the program
+        else:
+            """ Break on the first loop, because the first result will always be the first one """
+            break
+
+    """ Get the hyperlink encased within the quotes """
+    first_hyperlink = res_text[first_quote+1:first_quote+dif]
+    answer['article_src'] = first_hyperlink
+    print(first_hyperlink)
+
+    """ Navigate to the fetched hyperlink """
+    response = requests.get(url=first_hyperlink)
+    res_text = response.text
+
+    """ 
+    Find the case by states picture, src should contains "bm-kes-positif-negeri"
+    """
+    img_pattern = re.compile("<img.*src=\".*bm-kes-positif-negeri.*\"")
+    match = img_pattern.search(res_text)
+
+    
+    if match is not None:
+        link_pattern = re.compile("src=\".*bm-kes-positif-negeri.*\"")
+        link = link_pattern.search(match[0])
+        if link is not None:
+            """ Find the first and last quote that contains the link to the latest infographic """
+            first_quote = link[0].find("\"")
+            end_quote = link[0].find("\"",first_quote+1)
+
+            dif = end_quote - first_quote
+
+            """ Here is the link to the infographic """
+            src_link = link[0][first_quote+1:first_quote+dif]
+            if src_link != "":
+                print(src_link)
+                answer['image_src'] = src_link
+    else:
+        answer['image_src'] = "https://i.stack.imgur.com/6M513.png"
+
+
+
+    """ Find new cases, death count, cured count """
+    temp = None
+    output = ["cured","new","death"]
+
+    # keywords1 = [
+    #     "((Jumlah[ ]*(&nbsp;)*[ ]*kumulatif[ ]*(&nbsp;)*[ ]*kes[ ]*(&nbsp;)*[ ]*yang[ ]*(&nbsp;)*[ ]*telah[ ]*(&nbsp;)*[ ]*pulih[ ]*(&nbsp;)*[ ]*sepenuhnya[ ]*(&nbsp;)*[ ]*dari[ ]*(&nbsp;)*[ ]*COVID-19)|(Jumlah[ ]*(&nbsp;)*[ ]*kumulatif[ ]*(&nbsp;)*[ ]*kes[ ]*(&nbsp;)*[ ]*sembuh[ ]*(&nbsp;)*sepenuhnya[ ]*(&nbsp;)*[ ]*daripada[ ]*(&nbsp;)*[ ]*COVID-19))",
+    #     "((jumlah[ ]*(&nbsp;)*[ ]*kes[ ]*(&nbsp;)*[ ]*positif[ ]*(&nbsp;)*[ ]*COVID-19[ ]*(&nbsp;)*[ ]*di[ ]*(&nbsp;)*[ ]*Malaysia)|(kes[ ]*(&nbsp;)*[ ]*positif[ ]*(&nbsp;)*[ ]*COVID-19[ ]*(&nbsp;)*[ ]*di[ ]*(&nbsp;)*[ ]*Malaysia))",
+    #     "(jumlah[ ]*(&nbsp;)*[ ]*kumulatif[ ]*(&nbsp;)*[ ]*kes[ ]*(&nbsp;)*[ ]*kematian[ ]*(&nbsp;)*[ ]*COVID-19[ ]*(&nbsp;)*[ ]*di[ ]*(&nbsp;)*[ ]*Malaysia)"
+    #     ]
+
+    # Total Cured Count -> new cases -> death count
+    keywords1 = [
+        "Kes sembuh[ ]*(&nbsp;)*[ ]*",
+        "Kes baharu[ ]*(&nbsp;)*[ ]*",
+        "Kes kematian[ ]*(&nbsp;)*[ ]*"
+    ]
+
+    # keywords2 = "kes"
+    keywords2 = "kumulatif"
+    try:
+        for n in range(0,len(keywords1)):
+
+            print(n,end=" - ")
+            
+            # First filter
+            re_unit = re.compile(
+                pattern="(%s.{0,50}%s)"%(keywords1[n],keywords2),
+                flags=re.IGNORECASE
+            )
+            match_obj = re_unit.search(res_text)
+            temp = match_obj[0]
+            print(match_obj[0])
+
+            # Second filter, filtering html tags
+            re_unit = re.compile("(<[a-z]+>[ ]*\([ ]*[\d+[ ]*([, ]+\d+){0,}[ ]*</[a-z]+>[ ]*.*kes[ ]*<)|(<[a-z]+>[ ]*\([ ]*\d+[ ]*([, ]+\d+){0,}[ ]*</[a-z]+>[ ]*.*kes)|(\([ ]*\d+[ ]*([, ]+\d+){0,}(&nbsp;)*[ ]*kes)")
+            match_obj = re_unit.search(temp)
+            print(match_obj[0])
+
+            # Third filter
+            re_unit = re.compile('\d')
+            match_obj = re_unit.findall(match_obj[0])
+
+            if match_obj:
+                answer[output[n]] = int("".join(a for a in match_obj))
+
+        print(answer)
+        """ Return the fetched data and infographic url the the main program """
+        return answer
+
+    except Exception as e:
+        raise(e)
+
+
+
+
+
